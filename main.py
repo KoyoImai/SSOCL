c699145065fc091a83aa8d2c5b21c8fd855702f8
KoyoImai <imaikoyo0314@gmail.com>
2025-10-04 07:51:54 +0000

    add a new data

--- main.py
+++ main.py
@@ -17,6 +17,37 @@ from models import make_model
 
 
 
+
+def preparation(cfg):
+
+    # データセット毎にタスク数・タスク毎のクラス数を決定
+    # （現状不要）
+
+    # 総タスク数
+    # （現状不要）
+
+    # モデルの保存，実験記録などの保存先パス
+    if cfg.dataset.data_folder is None:
+        cfg.dataset.data_folder = '~/data/'
+    cfg.log.model_path = f'./logs/{cfg.method.name}/{cfg.log.name}/model/'      # modelの保存先
+    cfg.log.explog_path = f'./logs/{cfg.method.name}/{cfg.log.name}/exp_log/'   # 実験記録の保存先
+    cfg.log.mem_path = f'./logs/{cfg.method.name}/{cfg.log.name}/mem_log/'      # リプレイバッファ内の保存先
+    cfg.log.result_path = f'./logs/{cfg.method.name}/{cfg.log.name}/result/'    # 結果の保存先
+
+    # ディレクトリ作成
+    if not os.path.isdir(cfg.log.model_path):
+        os.makedirs(cfg.log.model_path)
+    if not os.path.isdir(cfg.log.explog_path):
+        os.makedirs(cfg.log.explog_path)
+    if not os.path.isdir(cfg.log.mem_path):
+        os.makedirs(cfg.log.mem_path)
+    if not os.path.isdir(cfg.log.result_path):
+        os.makedirs(cfg.log.result_path)
+
+
+
+
+
 @hydra.main(config_path='configs/default/', config_name='default', version_base=None)
 def main(cfg):
 
@@ -26,10 +57,20 @@ def main(cfg):
     seed_everything(cfg.seed)
 
 
+    # logの名前
+    cfg.log.name = f"{cfg.log.base}_{cfg.method.name}_{cfg.continual.mem_type}{cfg.continual.mem_size}_{cfg.dataset.type}_seed{cfg.seed}_date{cfg.date}"
+
+
+    # ===========================================
+    # データローダ作成やディレクトリ作成などの前処理
+    # ===========================================
+    preparation(cfg)
+
+
+
     # ===========================================
     # DDP
     # ===========================================
-    
     # DDP 使用の環境変数
     local_rank = int(os.environ["LOCAL_RANK"])
     use_ddp = local_rank != -1
@@ -49,15 +90,6 @@ def main(cfg):
 
 
 
-    # ===========================================
-    # 実験記録などを保存するディレクトリの作成
-    # ===========================================
-    cfg.log.model_dir = os.path.join(cfg.log.base_dir, cfg.log.name, cfg.log.model)
-    cfg.log.result_dir = os.path.join(cfg.log.base_dir, cfg.log.name, cfg.log.result)
-    os.makedirs(cfg.log.model_dir, exist_ok=True)
-    os.makedirs(cfg.log.result_dir, exist_ok=True)
-
-
 
     # ===========================================
     # modelの作成
@@ -66,9 +98,9 @@ def main(cfg):
 
 
 
-    # ===========================================
-    # データローダーの作成
-    # ===========================================
+    # # ===========================================
+    # # データローダーの作成
+    # # ===========================================
 
 
 
--- models/__init__.py
+++ models/__init__.py
@@ -7,15 +7,8 @@ from models.resnet_cifar_empssl import ResNet_Projection
 
 def make_model(cfg):
 
-    if (cfg.model.type == "resnet18_empssl") and (cfg.dataset.type in ["cifar10", "cifar100"]):
-
-        model = ResNet_Projection(name='resnet50', seed=777, cfg=cfg)
+    model = None
     
-    else:
-        model = None
-        assert False
-
-    print("model: ", model)
 
 
 
