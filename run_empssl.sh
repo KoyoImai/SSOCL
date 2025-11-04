515eb5060469229f9ab25f895e34eede5db67f7d
KoyoImai <imaikoyo0314@gmail.com>
2025-11-04 05:11:48 +0000

    add a new data

--- run_empssl.sh
+++ run_empssl.sh
@@ -8,5 +8,6 @@ export NCCL_ASYNC_ERROR_HANDLING=1
 export TORCH_NCCL_HEARTBEAT_TIMEOUT_SEC=1200
 
 
+
 # python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py --config-path ./configs/default/ --config-name default_empssl2
 python main_linear.py --config-path ./configs/default/ --config-name default_empssl2
